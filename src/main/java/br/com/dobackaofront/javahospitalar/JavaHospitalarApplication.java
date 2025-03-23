3522d1cb62f0976cc2bbcb883eeaad96d40ac0b4
Olival Paulino <olivalpaulinocc@gmail.com>
2025-03-23 14:32:23 -0300

    projeto criado

---
+++ src/main/java/br/com/dobackaofront/javahospitalar/JavaHospitalarApplication.java
@@ -0,0 +1,13 @@
+package br.com.dobackaofront.javahospitalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHospitalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHospitalarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dobackaofront/javahospitalar/JavaHospitalarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dobackaofront.javahospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHospitalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
